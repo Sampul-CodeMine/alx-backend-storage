e1aef3add87c64d4002cf88840b45508f0e07743
Sampul-CodeMine <studysampul@gmail.com>
2024-03-21 13:05:13 +0100

    Updates on Code

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -7,6 +7,5 @@ CREATE TABLE IF NOT EXISTS `users`(
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `name` VARCHAR(255),
   `country` ENUM('US', 'CO', 'TS') NOT NULL DEFAULT 'US'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+);
 COMMIT;
