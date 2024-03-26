29d4abf3b869b88785ed81790345c31c180e6a9a
Dukeson Ehigboria <studysampul@gmail.com>
2024-03-26 17:57:23 +0100

    Solution to Task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This is a module to update documents in a collection
+based on the name using pymongo
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    This is a method that updates documents in a collection
+    using python's pymongo library (python and mongodb) based on the search
+    criteria (names)
+    Args:
+        mongo_collection (pymongo)
+        name (str) to search for
+        topics (list of strings)
+    """
+    mongo_collection.update_many({"name": name},
+                                 {"$set": {"topics": topics}})
