d1cee87bf43cee630fc05e36313a933e65a1936c
Sampul-CodeMine <studysampul@gmail.com>
2024-03-26 14:31:46 +0100

    Added some scripts and files

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This is a module to list all documents in a collection
+using pymongo
+"""
+
+
+def list_all(mongo_collection):
+    """
+    This is a method that list all documents in a collection
+    using python's pymongo library (python and mongodb)
+    """
+    return [doc for doc in mongo_collection.find() if mongo_collection]
