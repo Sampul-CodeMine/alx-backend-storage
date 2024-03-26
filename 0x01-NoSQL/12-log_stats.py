4d7176757a652813b205313903a85474f3f124ea
Dukeson Ehigboria <studysampul@gmail.com>
2024-03-26 18:58:37 +0100

    Solution to Task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module for Task 12"""
+from pymongo import MongoClient
+
+
+ALLOWED_METHODS = ['DELETE', 'GET', 'PATCH', 'POST', 'PUT']
+
+
+def log_nginx_stats(mongo_collection, option=None):
+    """
+    This scripts provides statistics about NGINX logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
+    print("Methods:")
+    for method in ALLOWED_METHODS:
+        log_nginx_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == '__main__':
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_nginx_stats(nginx_collection)
