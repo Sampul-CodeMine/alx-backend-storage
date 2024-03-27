89d36bba070b3579ffacc9eed11a50b28acfd73f
Sampul-CodeMine <studysampul@gmail.com>
2024-03-27 17:37:42 +0100

    Solution to Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,12 +19,12 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        This is a method that stores data using a random key generated from
-        the uuid library:
+        This is a method that stores data using a random key generated
+        from the uuid library:
 
         Args:
-            data (str | bytes | int | float) - input data to be written to the
-            redis database
+            data (str | bytes | int | float) - input data to be written
+            to the redis database
 
         Returns:
             String (str) result
@@ -32,3 +32,45 @@ class Cache:
         u_id = str(uid.uuid4())
         self._redis.set(u_id, data)
         return u_id
+
+    def get(self, key: str, fn: Callable = None) \
+            -> Union[str, bytes, int, float]:
+        """
+        The is a method that gets data from Redis and converts it into
+        python type
+
+        Args:
+            key (str) - the key of the data to fetch from redis
+            fn (Callable function) - function to call
+
+        Returns:
+            (str | float | int | bytes) any of these datatypes
+        """
+        result = self._redis.get(key)
+        if fn:
+            return fn(result)
+        return result
+
+    def get_str(self, key: str) -> str:
+        """
+        This is a method that retrieves string data from the redis storage
+
+        Args:
+            key (str) - the key of the data to fetch
+
+        Returns:
+            String data (str)
+        """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        This is a method that retrieves integer data from the redis storage
+
+        Args:
+            key (str) - the key of the data to fetch
+
+        Returns:
+            Integer data (int)
+        """
+        return self.get(key, lambda x: int(x))
